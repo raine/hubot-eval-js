26c6ce0ab230954101c1f534f42376dbccf48780
Raine Virta <raine.virta@gmail.com>
2017-03-09 01:48:44 +0200

    add console.log support

--- src/index.js
+++ src/index.js
@@ -2,18 +2,28 @@
 //   eval
 
 const inspect = require('util').inspect
-const { concat, mergeAll, pipe, ifElse, test } = require('ramda')
-const { mdCode, mdPre } = require('./markdown')
+const { concat, ifElse, isEmpty, join, mergeAll, pipe, reject, test } = require('ramda')
+const { mdCode, mdPre, mdHeader } = require('./markdown')
 const S = require('sanctuary')
 const R = require('ramda')
 const RF = require('ramda-fantasy')
 const vm = require('vm')
 
 const evalCode = (str) => {
-  const sandbox = mergeAll([ { R, S }, R, RF ])
-  return vm.runInNewContext(str, sandbox, {
-    timeout: 10000
-  })
+  const output = []
+  const console = {
+    log: (arg) => {
+      output.push(arg)
+      return undefined
+    }
+  }
+  const sandbox = mergeAll([ { R, S, console }, R, RF ])
+  return {
+    value: vm.runInNewContext(str, sandbox, {
+      timeout: 10000
+    }),
+    output: output
+  }
 }
 
 const nlMdCode = lang => pipe(mdCode(lang), concat('\n'))
@@ -22,10 +32,24 @@ const inspectInfinite = (val) => inspect(val, { depth: Infinity })
 const getErrorMessage = (e) => e.message || String(e)
 const formatValueToReply = pipe(inspectInfinite, nlMdCode('js'))
 const formatErrorToReply = pipe(getErrorMessage, ifElse(isMultiline, nlMdCode('text'), mdPre))
+
+const formatOutput = (arr) =>
+  join('\n', [
+    mdHeader(4, 'output'),
+    mdCode('', join('\n', arr))
+  ])
+
+const formatReply = (res) => {
+  return join('\n', reject(isEmpty, [
+    formatValueToReply(res.value),
+    isEmpty(res.output) ? '' : formatOutput(res.output)
+  ]))
+}
+
 const readEvaluateAndPrint = (res) => {
   try {
     // TODO: res.reply mentions user which is kind of useless
-    res.reply(formatValueToReply(evalCode(res.match[1])))
+    res.reply(formatReply(evalCode(res.match[1])))
   } catch (e) {
     res.reply(formatErrorToReply(e))
   }
--- src/markdown.js
+++ src/markdown.js
@@ -1,4 +1,5 @@
-const { curry } = require('ramda')
+const { curry, join, pipe, repeat } = require('ramda')
+const repeatStr = pipe(repeat, join(''))
 
 const wrap = curry((x, s) => x + s + x)
 exports.mdLink = curry((text, url) => `[${text}](${url})`)
@@ -6,3 +7,4 @@ exports.mdBold = wrap('**')
 exports.mdStrike = wrap('~~')
 exports.mdPre = wrap('`')
 exports.mdCode = curry((lang, str) => '```' + lang + '\n' + str + '\n```')
+exports.mdHeader = (n, text) => [ repeatStr('#', n), text ].join(' ')
